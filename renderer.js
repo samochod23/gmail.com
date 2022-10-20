5680f19f43a49ff6dfbe42145021eca196fb424d
Trawa <63326146+samochod23@users.noreply.github.com>
2022-10-20 19:27:26 +0100

    Initial commit

---
+++ renderer.js
@@ -0,0 +1,6 @@
+imii adres IPfe80::4053:8fff:eff:4995
+192.168.1.2
+refresh system StaticRange.prototype
+numer komp.IPfe8052.1.A.3.137
+ImageBitmap(gniazdo  SIM1)
+358542091077064\\CountQueuingStrategy\\system reboot\\XPathExpression1nov.2020.start.XPathExpression1nov
